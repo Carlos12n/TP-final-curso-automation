20b1c3d86e585bc6916a0b3b837186ff8bcf7fb5
navarro754 <navarro754@gmail.com>
2024-02-18 18:18:43 -0300

    Resolucion practica 7 clase 3

---
+++ src/main/java/com/canavarro/clase5/P7clase3.java
@@ -0,0 +1,37 @@
+package com.canavarro.clase5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class P7clase3 {
+    /*
+Ingresar por consola un número mientras sea menor o igual a 500 y
+almacenar el número en una lista. Al finalizar el ingreso mostrar los
+datos y la cantidad ingresada por pantalla.
+
+También, sí hubo menos de 10 ingresos mostrar “Se ingresaron pocos
+números” de los contrario mostrar “Se excedió la cantidad de ingresos”
+         */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Integer numero; //equivale int
+
+        List<Integer> listaDeNumeros = new ArrayList<>();
+
+        do {
+            System.out.println("Ingrese un numero: ");
+            numero = scanner.nextInt();
+            listaDeNumeros.add(numero);
+        } while (numero <= 500);
+
+        System.out.println("Datos ingresados: "+listaDeNumeros);
+        System.out.println("Cantidad ingresada: "+listaDeNumeros.size());
+
+        if (listaDeNumeros.size() < 10){
+            System.out.println("Se ingresaron pocos numeros");
+        } else {
+            System.out.println("Se excedió la cantidad de ingresos");
+        }
+    }
+}
