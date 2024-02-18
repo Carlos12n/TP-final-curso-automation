0e84dcec71b8a36599ad56092f7219eef39a175c
navarro754 <navarro754@gmail.com>
2024-02-18 10:04:29 -0300

    Se agregan otros archivos

---
+++ src/main/java/com/canavarro/clase1/P1HolaMundo.java
@@ -0,0 +1,10 @@
+package com.canavarro.clase1;
+
+public class P1HolaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+
+    }
+}
---
+++ src/main/java/com/canavarro/clase1/P2imprimirDatos.java
@@ -0,0 +1,9 @@
+package com.canavarro.clase1;
+
+public class P2imprimirDatos {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println(10);
+        System.out.println(10.25);
+    }
+}
