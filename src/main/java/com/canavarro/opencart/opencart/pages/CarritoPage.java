db3587b10a57c52fc00936636498eeb8178184c8
navarro754 <navarro754@gmail.com>
2024-03-24 10:50:47 -0300

    Se agraga CarritoPage

---
+++ src/main/java/com/canavarro/opencart/opencart/pages/CarritoPage.java
@@ -0,0 +1,9 @@
+package com.canavarro.opencart.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CarritoPage extends BasePage{
+    public CarritoPage(WebDriver driver) {
+        super(driver);
+    }
+}
